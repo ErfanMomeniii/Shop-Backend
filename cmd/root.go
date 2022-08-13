16718b1ceb713da91d94c705ff7ac157260928fa
Erfan Momeni <Erfan.Momeni@snapp.cab>
2022-08-13 23:27:46 +0430

    add cobra command for root of project

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,19 @@
 package cmd
 
-func Execute(){
-	
-}
+import (
+	"github.com/spf13/cobra"
+	"fmt")
+var rootCmd = &cobra.Command{
+	Use:   "shop",
+	Short: "this project is the shop backend",
+	Long: `this project is the shop backend
+				  Complete documentation is available at https://github.com/ErfanMomeniii/Shop-Backend`,
+	Run: func(cmd *cobra.Command, args []string) {
+	},
+  }
+  
+  func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+	  fmt.Println(err)
+	}
+  }
