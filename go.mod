7d60215bd6f69b288180d1b9cdb6583faeef7cab
Erfan Momeni <Erfan.Momeni@snapp.cab>
2022-08-13 22:56:30 +0430

    create project

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ErfanMomeniii/Shop-Backend
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/ErfanMomeniii/Shop-Backend/cmd"
+)
+
+func main() {
+	cmd.Execute();
+}
